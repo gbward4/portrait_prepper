197dcfe21552c53aecbbcb4b7b6da6beab0cf12c
gward01 <gb.ward@molex.com>
2023-12-20 09:35:35 -0600

    Added reverse feature, label, killed some bugs

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ import sys
 import cv2
 import numpy as np
 from PIL import Image, ImageFilter, ImageDraw, ImageEnhance, ImageOps, ImageQt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QSlider, QVBoxLayout, QWidget, QAction, QFileDialog, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QSlider, QVBoxLayout, QWidget, QAction, QFileDialog, QHBoxLayout, QCheckBox
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import Qt, pyqtRemoveInputHook
 
@@ -135,37 +135,49 @@ class PictureEditor(QMainWindow):
         self.image_label.setAlignment(Qt.AlignCenter)
 
         # Original Image Display
-        self.orig_image_label = QLabel(self)
-        self.orig_image_label.setAlignment(Qt.AlignCenter)
+        self.reference_image_label = QLabel(self)
+        self.reference_image_label.setAlignment(Qt.AlignCenter)
 
         # Histogram Display
         self.histogram_label = QLabel(self)
         self.histogram_label.setAlignment(Qt.AlignCenter)
 
-        # Sliders
-        self.shadow_slider = QSlider(Qt.Horizontal)
-        self.shadow_slider.setRange(0, 100)
-        self.shadow_slider.setValue(25)
-        self.shadow_slider.valueChanged.connect(self.update_image)
+        # Sliders        
+        self.shadow_slider = self.create_labeled_slider("Shadow:", Qt.Horizontal)
+        self.shadow_slider.slider.setRange(0, 100)
+        self.shadow_slider.slider.setValue(25)
+        self.shadow_slider.slider.valueChanged.connect(self.update_image)
 
-        self.midtone_slider = QSlider(Qt.Horizontal)
-        self.midtone_slider.setRange(0, 100)
-        self.midtone_slider.setValue(50)
-        self.midtone_slider.valueChanged.connect(self.update_image)
+        self.midtone_slider = self.create_labeled_slider("Midtone:", Qt.Horizontal)
+        self.midtone_slider.slider.setRange(0, 100)
+        self.midtone_slider.slider.setValue(50)
+        self.midtone_slider.slider.valueChanged.connect(self.update_image)
 
-        self.fleshtone_slider = QSlider(Qt.Horizontal)
-        self.fleshtone_slider.setRange(0, 100)
-        self.fleshtone_slider.setValue(75)
-        self.fleshtone_slider.valueChanged.connect(self.update_image)
+        self.fleshtone_slider = self.create_labeled_slider("Fleshtone:", Qt.Horizontal)
+        self.fleshtone_slider.slider.setRange(0, 100)
+        self.fleshtone_slider.slider.setValue(75)
+        self.fleshtone_slider.slider.valueChanged.connect(self.update_image)
+
+        # Buttons
+        self.checkbox_reverse = QCheckBox("Reverse Image", self)
+        self.checkbox_reverse.setChecked(True)
+        self.checkbox_reverse.stateChanged.connect(self.checkbox_reverse_state_changed)
 
         # Layout
         central_widget = QWidget(self)
         main_layout = QVBoxLayout(central_widget)
 
+        settings_layout = QVBoxLayout() 
+        settings_layout.addWidget(self.checkbox_reverse)
+        settings_layout.addWidget(self.histogram_label)
+
+        images_layout = QHBoxLayout()
+        images_layout.addWidget(self.image_label)
+        images_layout.addWidget(self.reference_image_label)
+
         labels_layout = QHBoxLayout()
-        labels_layout.addWidget(self.image_label)
-        labels_layout.addWidget(self.orig_image_label)
-        labels_layout.addWidget(self.histogram_label)
+        labels_layout.addLayout(images_layout)
+        labels_layout.addLayout(settings_layout)
 
         sliders_layout = QVBoxLayout()
         sliders_layout.addWidget(self.shadow_slider)
@@ -182,6 +194,32 @@ class PictureEditor(QMainWindow):
         self.setWindowTitle('Picture Editor')
         self.setGeometry(100, 100, 800, 600)
 
+    def create_labeled_slider(self, label_text, orientation):
+        # Create a QLabel for the slider label
+        label = QLabel(label_text)
+
+        # Create a QSlider
+        slider = QSlider(orientation)
+        slider.setRange(0, 100)
+        slider.setValue(50)  # Default value
+
+        # Create a layout for the label and slider
+        slider_layout = QVBoxLayout()
+        slider_layout.addWidget(label)
+        slider_layout.addWidget(slider)
+
+        # Create a container widget to hold the label and slider layout
+        container_widget = QWidget()
+        container_widget.setLayout(slider_layout)
+        container_widget.slider = slider
+
+        return container_widget
+
+    def checkbox_reverse_state_changed(self):
+        self.original_image = self.original_image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+        self.display_reference_image(self.original_image)
+        self.update_image()
+
     def open_image(self):
         file_dialog = QFileDialog()
         file_path, _ = file_dialog.getOpenFileName(self, 'Open Image', '', 'Image Files (*.png *.jpg *.jpeg *.bmp *.gif)')
@@ -189,11 +227,13 @@ class PictureEditor(QMainWindow):
         if file_path:
             self.original_image = Image.open(file_path)
             # TODO Hack
-            self.original_image = self.original_image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
             self.original_image = ImageOps.autocontrast(self.original_image, 0.0)
 
+            if self.checkbox_reverse.isChecked():
+                self.checkbox_reverse_state_changed()
+
             self.display_image(self.original_image)
-            self.display_orig_image(self.original_image)
+            self.update_image()
 
     def save_image(self):
         # Todo handle no image
@@ -222,9 +262,9 @@ class PictureEditor(QMainWindow):
         pixmap = QPixmap.fromImage(qim)
         return pixmap       
 
-    def display_orig_image(self, image):
+    def display_reference_image(self, image):
         pixmap = self.pil2pixmap(image)
-        self.orig_image_label.setPixmap(pixmap)
+        self.reference_image_label.setPixmap(pixmap)
 
     def display_image(self, image):
         pixmap = self.pil2pixmap(image)
@@ -236,9 +276,9 @@ class PictureEditor(QMainWindow):
 
     def update_histogram(self):
         threshold_lines = [
-                self.shadow_slider.value(),
-                self.midtone_slider.value(),
-                self.fleshtone_slider.value(),
+                self.shadow_slider.slider.value(),
+                self.midtone_slider.slider.value(),
+                self.fleshtone_slider.slider.value(),
                 ]
 
         self.histogram = get_histogram(self.original_image, threshold_lines)
@@ -249,9 +289,9 @@ class PictureEditor(QMainWindow):
         if not self.original_image:
             return
 
-        shadow_layer = Layer(name="shadow", raw_image=self.original_image, threshold_percent=self.shadow_slider.value(), canvas_color=(0, 0, 0))
-        midtone_layer = Layer(name="midtone", raw_image=self.original_image, threshold_percent=self.midtone_slider.value(), canvas_color=(0x58, 0x09, 0x9C))
-        fleshtone_layer = Layer(name="fleshtone", raw_image=self.original_image, threshold_percent=self.fleshtone_slider.value(), canvas_color=(0xFF, 0xAE, 0))
+        shadow_layer = Layer(name="shadow", raw_image=self.original_image, threshold_percent=self.shadow_slider.slider.value(), canvas_color=(0, 0, 0))
+        midtone_layer = Layer(name="midtone", raw_image=self.original_image, threshold_percent=self.midtone_slider.slider.value(), canvas_color=(0x58, 0x09, 0x9C))
+        fleshtone_layer = Layer(name="fleshtone", raw_image=self.original_image, threshold_percent=self.fleshtone_slider.slider.value(), canvas_color=(0xFF, 0xAE, 0))
 
         composite_image = Image.new('RGBA', shadow_layer.size, (255, 255, 255, 255))
         composite_image = Image.composite(composite_image, fleshtone_layer.colored_canvas, fleshtone_layer.threshold_mask)
@@ -273,7 +313,6 @@ if __name__ == '__main__':
 
 
 # TODO
-# Toggle button flip
 # Toggle Heatmap presets
 # MVC the thing
 # Documentation
